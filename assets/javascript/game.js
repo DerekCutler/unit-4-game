e57bc3f54804495a374eff2f5bc98690507cac58
Derek P. Cutler <43916483+DerekCutler@users.noreply.github.com>
2018-11-10 13:25:25 -0700

    Inital setup

---
+++ assets/javascript/game.js
@@ -0,0 +1,28 @@
+
+// Show in HTML under: <div id="guessTheScotch"></div>
+document.getElementById("jQueryMagic").innerHTML = TBD;
+// 
+
+
+
+// console.log(TESTTHATMF)
+
+// WORKS UPTO THIS
+
+//** */FIN**
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ assets/javascript/test.js
@@ -0,0 +1,1 @@
+// Ransdom sandbox of test stuff.
